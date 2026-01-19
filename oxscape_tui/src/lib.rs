f6f7c8266100aac72ab2dd0ab9192f903b923b03
Fee Fladder <freakyfee@proton.me>
2026-01-19 14:40:11 +0100

    tui:tweaked colors, refactored sim

--- oxscape_tui/src/lib.rs
+++ oxscape_tui/src/lib.rs
@@ -1,5 +1,14 @@
-use oxscape::{GridMeta, Result};
-use oxscape_erode::fill_deps::priority_flood_wei2018;
+use color_eyre::{Result, eyre::Report};
+use colorous::Gradient;
+use oxscape::{
+    GridMeta,
+    mflow::{Order, metrics::Dinf},
+};
+use oxscape_erode::{
+    Params, add_uplift,
+    fill_deps::priority_flood_wei2018,
+    mflow::{accum, erode},
+};
 use rand::prelude::*;
 
 /// Arrows that point in the direction
@@ -33,5 +42,99 @@ pub fn random_dem(dem: &mut [f64], meta: &GridMeta, seed: u64) -> Result<()> {
                 row[i] = rng.random_range(0.0..1.0);
             }
         });
-    priority_flood_wei2018(dem, &meta)
+    priority_flood_wei2018(dem, meta).map_err(|e| Report::msg(e.to_string()))
+}
+
+pub trait Simulation: Sized {
+    /// Initialize the simulation to the given size
+    fn init(width: usize, height: usize, gradient: Gradient) -> Result<Self>;
+    /// Resize the simulation, maybe also restarts it
+    fn resize(&mut self, width: usize, height: usize) -> Result<()>;
+    /// Restart the simulation
+    fn restart(&mut self) -> Result<()>;
+    /// Do a single simulation step
+    fn step(&mut self) -> Result<()>;
+    /// Give access to the underlying dem
+    fn dem(&self) -> &[f64];
+    /// Give access to the order
+    ///
+    /// For metadata (width/height) and flow directions
+    fn order(&self) -> &Order;
+    /// Give access to the color gradient
+    fn gradient(&self) -> &Gradient;
+}
+
+pub struct DefaultSim {
+    gradient: Gradient,
+    dem: Vec<f64>,
+    accum: Vec<f64>,
+    order: Order,
+    params: Params,
+    seed: u64,
+}
+
+impl Simulation for DefaultSim {
+    fn init(width: usize, height: usize, gradient: Gradient) -> Result<Self> {
+        let meta = GridMeta::new(width, height);
+        let params = Params {
+            cell_area: 10000.0,
+            ..Default::default()
+        };
+
+        let mut dem = vec![0.0; meta.size()];
+        random_dem(&mut dem, &meta, 42).map_err(|e| Report::msg(e.to_string()))?;
+        let order = Order::from_dem_metric(meta, &dem, &mut Dinf)
+            .map_err(|e| Report::msg(e.to_string()))?;
+
+        Ok(Self {
+            gradient,
+            dem,
+            accum: vec![0.0; order.meta().size()],
+            order,
+            params,
+            seed: 42,
+        })
+    }
+
+    fn resize(&mut self, width: usize, height: usize) -> Result<()> {
+        let meta = GridMeta::new(width, height);
+        self.dem.resize(meta.size(), 0.0);
+        random_dem(&mut self.dem, &meta, self.seed).map_err(|e| Report::msg(e.to_string()))?;
+        self.accum.resize(meta.size(), 0.0);
+        self.order = Order::from_dem_metric(meta, &self.dem, &mut Dinf)
+            .map_err(|e| Report::msg(e.to_string()))?;
+        Ok(())
+    }
+
+    fn restart(&mut self) -> Result<()> {
+        self.seed += 1;
+        random_dem(&mut self.dem, self.order.meta(), self.seed)
+            .map_err(|e| Report::msg(e.to_string()))?;
+        self.order
+            .reorder(&self.dem, &mut Dinf)
+            .map_err(|e| Report::msg(e.to_string()))
+    }
+
+    fn step(&mut self) -> Result<()> {
+        add_uplift(self.order.meta(), &self.params, &mut self.dem);
+        accum(&self.order, &self.params, &mut self.accum);
+        erode(&self.order, &self.params, &self.accum, &mut self.dem);
+        priority_flood_wei2018(&mut self.dem, self.order.meta())
+            .map_err(|e| Report::msg(e.to_string()))?;
+        self.order
+            .reorder(&self.dem, &mut Dinf)
+            .map_err(|e| Report::msg(e.to_string()))
+    }
+
+    fn dem(&self) -> &[f64] {
+        &self.dem
+    }
+
+    fn gradient(&self) -> &Gradient {
+        &self.gradient
+    }
+
+    fn order(&self) -> &Order {
+        &self.order
+    }
 }
--- oxscape_tui/src/main.rs
+++ oxscape_tui/src/main.rs
@@ -1,129 +1,115 @@
-//! 
+//! Terminal User Interface (TUI) for debugging purposes
+//!
+//! runs the model in full-size terminal.
+//!
+//! Arrows (max 2) indicate flow directions and their colors the levels. All
+//! arrows of the same color are run in parallel.
+//!  
 use std::time::Duration;
 
 use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode};
 use ordered_float::OrderedFloat;
-use oxscape::{
-    GridMeta,
-    mflow::{NO_FLOW_GEN, Order, metrics::Dinf},
-};
-use oxscape_erode::{
-    Params, add_uplift, fill_deps::priority_flood_wei2018, mflow::{accum, erode}
-};
-use oxscape_tui::{DIRS, random_dem};
+use oxscape::mflow::NO_FLOW_GEN;
+use oxscape_tui::{DIRS, DefaultSim, Simulation};
 use ratatui::style::Color::Rgb;
 use rayon::prelude::*;
 
 fn main() -> Result<()> {
-    let mut params = Params::default();
     let mut play: bool = false;
-    let mut seed = 42;
-    params.cell_area = 10000.0;
     ratatui::run(|terminal| {
         let s = terminal.size()?;
-        let meta = GridMeta::new(usize::from(s.width / 2), usize::from(s.height));
-        let mut dem = vec![0.0; meta.size()];
-        let mut prev_dem = vec![0.0;meta.size()];
-        let mut acc = vec![0.0; meta.size()];
-        random_dem(&mut dem, &meta, seed).unwrap();
-        let mut order = Order::from_dem_metric(meta, &dem, &mut Dinf).unwrap();
+        let mut start_frame = terminal.get_frame().count();
+        let mut sim = DefaultSim::init(
+            usize::from(s.width / 2),
+            usize::from(s.height),
+            colorous::CUBEHELIX,
+        )?;
         loop {
             if event::poll(Duration::from_millis(0))? {
                 match event::read()? {
                     Event::Resize(width, height) => {
-                        let meta = GridMeta::new(usize::from(width/2), usize::from(height));
-                        dem = vec![0.0;meta.size()];
-                        random_dem(&mut dem, &meta, seed).unwrap();
-                        prev_dem = vec![0.0;meta.size()];
-                        acc = vec![0.0;meta.size()];
-                        order = Order::from_dem_metric(meta, &mut dem, &mut Dinf).unwrap();
+                        sim.resize(usize::from(width / 2), usize::from(height))?;
                     }
-                    Event::Key(k) => {
-                        match k.code {
-                            KeyCode::Enter => {
-                                // dem[order.meta().xy_to_i(2, 2)] = 20.0;
-                                random_dem(&mut dem, &order.meta(), seed).unwrap();
-                                order.reorder(&dem, &mut Dinf).unwrap();
-                                seed += 1;
-                                // continue;
-                            }
-                            KeyCode::Right => {
-                                accum(&order, &params, &mut acc);
-                                add_uplift(&order.meta(), &params, &mut dem);
-                                erode(&order, &params, &acc, &mut dem);
-                                priority_flood_wei2018(&mut dem, order.meta()).unwrap();
-                                order.reorder(&dem, &mut Dinf).unwrap();
-                            }
-                            KeyCode::Char(' ') => play = !play,
-                            _ => break Ok(()),
+                    Event::Key(k) => match k.code {
+                        KeyCode::Enter => {
+                            sim.restart()?;
                         }
-                    }
+                        KeyCode::Right => {
+                            sim.step()?;
+                        }
+                        KeyCode::Char(' ') => play = !play,
+                        _ => break Ok(()),
+                    },
                     _ => {}
                 }
             }
             terminal.draw(|frame| {
-                assert_eq!(usize::from(frame.area().width/2), order.meta().width());
-                assert_eq!(usize::from(frame.area().height), order.meta().height());
+                assert_eq!(
+                    usize::from(frame.area().width / 2),
+                    sim.order().meta().width()
+                );
+                assert_eq!(
+                    usize::from(frame.area().height),
+                    sim.order().meta().height()
+                );
                 let buf = frame.buffer_mut();
-                let max = dem.par_iter().map(|v| OrderedFloat(*v)).max().unwrap().0;
-                order
+                let max = sim
+                    .dem()
+                    .par_iter()
+                    .map(|v| OrderedFloat(*v))
+                    .max()
+                    .unwrap()
+                    .0;
+                sim.order()
                     .levels()
                     .windows(2)
-                    .map(|w| &order.stack()[w[0]..w[1]])
+                    .map(|w| &sim.order().stack()[w[0]..w[1]])
                     .enumerate()
                     .for_each(|(i, lvl)| {
                         for c in lvl {
-                            let (x, y) = order.meta().i_to_xy(*c);
-                            let color = colorous::VIRIDIS.eval_rational(order.n_levels()-i, order.n_levels());
+                            let (x, y) = sim.order().meta().i_to_xy(*c);
+                            let color = sim
+                                .gradient()
+                                .eval_rational(sim.order().n_levels() - i, sim.order().n_levels());
                             buf[(u16::try_from(x * 2).unwrap(), u16::try_from(y).unwrap())]
                                 .set_fg(Rgb(color.r, color.g, color.b));
                             buf[(u16::try_from(x * 2 + 1).unwrap(), u16::try_from(y).unwrap())]
                                 .set_fg(Rgb(color.r, color.g, color.b));
                         }
                     });
-                for y in 0..order.meta().height() {
-                    for x in 0..order.meta().width() {
-                        let n = order.meta().xy_to_i(x, y);
+                for y in 0..sim.order().meta().height() {
+                    for x in 0..sim.order().meta().width() {
+                        let n = sim.order().meta().xy_to_i(x, y);
                         let mut n_dirs = 0;
-                        for (idx, f) in order.flows()[n].iter().enumerate() {
+                        for (idx, f) in sim.order().flows()[n].iter().enumerate() {
                             if *f != NO_FLOW_GEN {
                                 if n_dirs >= 2 {
                                     // prevent overflow in multiflow
                                     continue;
                                 }
-                                buf[(u16::try_from(x * 2 + n_dirs).unwrap(), u16::try_from(y).unwrap())].set_char(DIRS[idx]);
+                                buf[(
+                                    u16::try_from(x * 2 + n_dirs).unwrap(),
+                                    u16::try_from(y).unwrap(),
+                                )]
+                                    .set_char(DIRS[idx]);
                                 n_dirs += 1;
                             }
                         }
-                        let bg = colorous::VIRIDIS.eval_continuous(dem[n] / max);
-                        buf[(u16::try_from(x * 2).unwrap(), u16::try_from(y).unwrap())].set_bg(Rgb(bg.r, bg.g, bg.b));
-                        buf[(u16::try_from(x * 2 + 1).unwrap(), u16::try_from(y).unwrap())].set_bg(Rgb(bg.r, bg.g, bg.b));
+                        let bg = sim.gradient().eval_continuous(sim.dem()[n] / max);
+                        buf[(u16::try_from(x * 2).unwrap(), u16::try_from(y).unwrap())]
+                            .set_bg(Rgb(bg.r, bg.g, bg.b));
+                        buf[(u16::try_from(x * 2 + 1).unwrap(), u16::try_from(y).unwrap())]
+                            .set_bg(Rgb(bg.r, bg.g, bg.b));
                     }
                 }
             })?;
             if play {
-                accum(&order, &params, &mut acc);
-                add_uplift(order.meta(), &params, &mut dem);
-                erode(&order, &params, &acc, &mut dem);
-                priority_flood_wei2018(&mut dem, order.meta()).unwrap();
-                order.reorder(&dem, &mut Dinf).unwrap();
-
-                // restart on steady state
-                if dem
-                    .par_iter()
-                    .zip(prev_dem.par_iter())
-                    .map(|(a, b)| OrderedFloat((a - b).abs()))
-                    .max()
-                    .unwrap()
-                    .0
-                    < 0.1
-                {
-                    random_dem(&mut dem, &order.meta(), seed).unwrap();
-                    order.reorder(&dem, &mut Dinf).unwrap();
-                    seed += 1;
+                sim.step()?;
+                if terminal.get_frame().count() - start_frame >= 128 {
+                    sim.restart()?;
+                    start_frame = terminal.get_frame().count();
                 }
-                prev_dem.copy_from_slice(&dem);
             }
         }
     })
